0e748155e8a017afb2614757d662d248512d0f81
ievas <ieva.antons@gmail.com>
2021-10-14 23:59:22 -0600

    minor fix

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ from sqlalchemy.exc import IntegrityError
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///panico"
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-#     'DATABASE_URL').replace("://", "ql://",1)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///panico"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL').replace("://", "ql://",1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 # app.config["SECRET_KEY"] = "las palabras"
